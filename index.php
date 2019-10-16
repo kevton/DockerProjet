6d96901a2e11f200f4d600b50c61bc1b8e3010f2
kevton <kevton@users.noreply.github.com>
2019-10-16 23:42:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+
+    mail("Il était une fois, un intervenant nommé Vincent qui avait un étudiant qu'il afféctionnait particulièrement. Pour celà, il lui donna la meilleure note à son TP. THE END", "TPDocker", "message", "From: Sender");
+
+?>
